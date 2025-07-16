5e1c0453f8d7550f713afc47a5682f3f7b685d90
PRISMdevaccount <diyaramakrishnan@delta.edu>
2025-07-16 01:18:35 +0000

    l

--- components/BlogSlider.jsx
+++ components/BlogSlider.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { getAllPosts } from '@/lib/blog';
+import { getAllPosts } from '../lib/blog';
 
 export default function BlogSlider() {
   const posts = getAllPosts();
