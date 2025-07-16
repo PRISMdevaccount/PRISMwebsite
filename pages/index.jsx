03a7093d07ebb2ff17fc496491d27cc892429c4b
PRISMdevaccount <diyaramakrishnan@delta.edu>
2025-07-16 01:09:48 +0000

    l

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
-import Navbar from '.../components/Navbar';
-import Hero from '.../components/Hero';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
 import BlogSlider from '.../components/BlogSlider';
-import Story from '.../components/Story';
-import Footer from '.../components/Footer';
+import Story from '../components/Story';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
